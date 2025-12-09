6b26dda2dce67772147bf09b373946286f09e6af
Daniel Marcos Serrano <danielmarcosserrano760@gmail.com>
2025-12-09 22:21:02 +0100

    add infinite scroll

---
+++ public/app.js
@@ -0,0 +1,10 @@
+
+
+function isEnd() { 
+
+    if (window.innerHeight + document.documentElement.scrollTop >= document.body.scrollHeight) { 
+        alert("Has llegado al final")
+    }
+        
+}
+
